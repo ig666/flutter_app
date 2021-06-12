daf41ed88569551565e88e34b040cb1539c0bbf4
Qi Chen <qi.chen>
2021-06-12 17:23:58 +0800

    modify

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,54 @@
 import 'package:flutter/material.dart';
+import './res/listData.dart' as list;
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  Widget _getList(BuildContext context, int index) {
+    return Card(
+        margin: EdgeInsets.all(10),
+        child: Container(
+          height: 300,
+          child: Column(
+            children: [
+              Expanded(
+                  flex: 2,
+                  child: Image.network(
+                    list.listData[index]["imageUrl"],
+                    fit: BoxFit.cover,
+                    width: double.infinity,
+                  )),
+              Expanded(
+                  flex: 1,
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage:
+                          NetworkImage(list.listData[index]["imageUrl"]),
+                    ),
+                    title: Text(list.listData[index]["title"]),
+                    subtitle: Text(
+                      list.listData[index]["description"],
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 4,
+                    ),
+                  ))
+            ],
+          ),
+        ));
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'flutter',
+      title: 'app',
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('flutter_demo'),
-        ),
-        body: GridView.count(
-          crossAxisCount: 2,
-          children: [Text('你说呢'), Text('我说你')],
-        ),
-      ),
+          appBar: AppBar(
+            title: Text('我的App'),
+          ),
+          body: ListView.builder(
+              itemCount: list.listData.length, itemBuilder: this._getList)),
     );
   }
-  // This widget is the root of your application.
-
 }
